13e50a422bddf316c9b87097083db75ba8ba1497
diggzhang <diggzhang@gmail.com>
2019-03-05 00:30:27 +0800

    Add sql

---
+++ sql/members.create.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS members;
+CREATE TABLE members (name text, times real);
+INSERT INTO members VALUES ('xingze', 1000);
+INSERT INTO members VALUES ('Barack Obama', 1000);
+SELECT * FROM members;
