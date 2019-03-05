78385719547a02f0eb15e57537c22919d39b5b69
diggzhang <diggzhang@gmail.com>
2019-03-05 19:20:45 +0800

    Add sign_in_db modulej

--- load_faces.py
+++ load_faces.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import glob
+import sqlite3
 import face_recognition
 
 
@@ -20,14 +21,28 @@ class FileInfo:
         return face_recognition.face_encodings(load_image)[0]
 
     def sign_in_db(self):
-        # TODO 如果用户没有登记过，给登记一下
-        pass
+        conn = sqlite3.connect('sign-in.db')
+        c = conn.cursor()
+        # Just be sure any changes have been committed or they will be lost.
+        sql_str = """
+            SELECT COUNT(1) FROM members WHERE NAME = '{name}'
+        """.format(name=self.get_name())
+        if c.execute(sql_str).fetchone()[0] == 0:
+            insert_str = """
+            INSERT INTO members VALUES ('{name}', 1000);
+            """.format(name=self.get_name())
+            c.execute(insert_str)
+        c.close()
+        conn.commit()
+        conn.close()
+
 
 def load_file_list(file_lst):
     known_face_encodings = []
     known_face_names = []
     for file in file_lst:
         f = FileInfo(file)
+        f.sign_in_db()
         known_face_encodings.append(f.get_known_face_encodings())
         known_face_names.append(f.get_name())
     return {"names": known_face_names, "encoding": known_face_encodings}
@@ -37,6 +52,12 @@ def face_db():
     return load_file_list(
         glob.glob(os.getcwd() + "/images/*")
     )
+
+
+def main():
+    # print(
+    face_db()
+    # )
 
 
 if __name__ == '__main__':
